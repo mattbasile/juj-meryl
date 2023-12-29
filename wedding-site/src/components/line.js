6d71c43204b9aadf2b6ef75208c199bde6ba7c0a
mattbasile <matthewbasile2@gmail.com>
2023-12-28 23:08:38 -0500

    clean up

--- wedding-site/src/components/line.js
+++ wedding-site/src/components/line.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
-export default function ({ color }) {
-  return (
-    <svg
-      width="1"
-      height="80"
-      viewBox="0 0 1 80"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <line
-        x1="0.75"
-        y1="80"
-        x2="0.749996"
-        y2="1.09278e-08"
-        stroke={color === 'light' ? '#FFFBF4' : '#8C6418'}
-        stroke-width="0.5"
-      />
-    </svg>
-  );
-}
+ const Line = ({ color }) => {
+   return (
+     <svg
+       width="1"
+       height="80"
+       viewBox="0 0 1 80"
+       fill="none"
+       xmlns="http://www.w3.org/2000/svg"
+     >
+       <line
+         x1="0.75"
+         y1="80"
+         x2="0.749996"
+         y2="1.09278e-08"
+         stroke={color === 'light' ? '#FFFBF4' : '#8C6418'}
+         stroke-width="0.5"
+       />
+     </svg>
+   );
+ };
+export default Line;
