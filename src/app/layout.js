a25c9f28befb12c1e736d52a6ccc1bc1580682a8
mattbasile <matthewbasile2@gmail.com>
2023-12-29 09:26:42 -0500

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css';
-
+import Head from 'next/head';
 export const metadata = {
   title: 'Meryl and Juliana',
   description: 'They are getting hitched!',
@@ -8,6 +8,34 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-icon-192x192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        />
+        <link rel="manifest" href="/favicon/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className="font-sweetsans">{children}</body>
     </html>
   );
